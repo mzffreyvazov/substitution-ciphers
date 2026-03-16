6a99469638516be1e92cc118f4d92036134150e0
mzffreyvazov <mzffreyvazov2005@gmail.com>
2026-03-16 19:48:24 +0400

    complete caeser cypher

--- src/main/java/CeaserCipher.java
+++ src/main/java/CeaserCipher.java
@@ -1,9 +1,50 @@
 public class CeaserCipher {
+
+    static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static String encrypt(String input, int shift) {
+        int alphabetLen = ALPHABET.length();
+
+        StringBuilder cypher = new StringBuilder();
+
+        for (int i = 0; i<input.length();  i++) {
+            int index = ALPHABET.indexOf(input.charAt(i));
+            char newChar = ALPHABET.charAt(index + shift);
+            cypher.append(newChar);
+
+        }
+
+        return cypher.toString();
+    }
+
+    public static String decrypt(String cypher, int shift) {
+        int alphabetLen = ALPHABET.length();
+
+        StringBuilder message = new StringBuilder();
+
+        for (int i = 0; i<cypher.length();  i++) {
+            int index = ALPHABET.indexOf(cypher.charAt(i));
+            char newChar = ALPHABET.charAt(index - shift);
+            message.append(newChar);
+
+        }
+
+        return message.toString();
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        String message = "TRAVELEAST";
+        int rotation = 3;
+
+        System.out.println(message);
+
+        String cypher = encrypt(message, rotation);
+        System.out.println(cypher);
+
+        String originalMessage = decrypt(cypher, rotation);
+        System.out.println(originalMessage);
 
 
     }
-
-    static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 }
