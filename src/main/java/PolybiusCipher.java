cdd40f7505b6b413e7feea4546483ded6fb63dd5
mzffreyvazov <mzffreyvazov2005@gmail.com>
2026-03-19 00:46:08 +0400

    add PolybiusCipher

---
+++ src/main/java/PolybiusCipher.java
@@ -0,0 +1,58 @@
+public class PolybiusCipher {
+
+    public static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+    public static String encrypt(String plainText) {
+
+        StringBuilder cipher = new StringBuilder();
+        for (int i = 0; i < plainText.length(); i++) {
+            char plainTextChar = plainText.charAt(i);
+            int plainTextIndex = ALPHABET.indexOf(plainTextChar);
+
+
+            if (plainTextIndex >= 9) {
+                plainTextIndex--;
+            }
+
+            int row = plainTextIndex / 5;
+            int col = plainTextIndex % 5;
+
+            cipher.append(Integer.toString(row));
+            cipher.append(Integer.toString(col));
+        }
+
+        return cipher.toString();
+    }
+
+    public static String decrypt(String cipher) {
+        StringBuilder message = new StringBuilder();
+
+        for (int i = 0; i < cipher.length(); i+=2) {
+            int row = Character.getNumericValue(cipher.charAt(i));
+            int col = Character.getNumericValue(cipher.charAt(i+1));
+
+            int messageIndex = 5 * row + col;
+            if (messageIndex >= 9) {
+                messageIndex++;
+            }
+            message.append(ALPHABET.charAt(messageIndex));
+
+        }
+
+        return message.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String message = "TRAVELEAST";
+        System.out.println(message);
+
+        String cipher = encrypt(message);
+        System.out.println(cipher);
+
+        String originalMessage = decrypt(cipher);
+        System.out.println(originalMessage);
+
+    }
+}
